e022b89e939a89856f94e2dc24eaea9c8f5e08e2
litianqi <871651575@qq.com>
2017-08-11 16:16:26 +0800

    Initial Commit

---
+++ VFLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VFLDemo
+//
+//  Created by litianqi on 2017/8/11.
+//  Copyright © 2017年 TQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VFLDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VFLDemo
+//
+//  Created by litianqi on 2017/8/11.
+//  Copyright © 2017年 TQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
